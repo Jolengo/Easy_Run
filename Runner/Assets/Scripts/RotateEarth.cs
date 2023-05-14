ffb6acd9877390c1dca53debb6ced31b9788c87c
Jolengo <danil.elimarov@mail.ru>
2023-05-14 22:53:24 +0300

    Commit_Platform
    платформы ловушки и пару скриптов

---
+++ Runner/Assets/Scripts/RotateEarth.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateEarth : MonoBehaviour
+{
+    [SerializeField] public float speed = 0; // скорость
+
+    void Update()
+    {
+        transform.Rotate(0, 0, speed * Time.deltaTime); // вращение планеты
+    }
+}
