417a65f725b958c2afff88a7de6bcf77224f9e0c
A5AL <62794056+A5AL@users.noreply.github.com>
2023-08-09 01:02:33 +0300

    Counter commit
    Сделал счетчик количества пройденных метров по оси Z (текст на канвасе + скрипт)

---
+++ Runner/Assets/Scenes/Ruslan/Scripts/PointCounter.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointCounter : MonoBehaviour
+{
+
+    public Transform PlayerPosition;
+    public Text Counter;
+
+    private float _playerPosZ;
+    private int _count = 0;
+
+    void Start()
+    {
+        _playerPosZ = PlayerPosition.position.z;
+    }
+
+    void Update()
+    {
+        if (PlayerPosition.position.z > _playerPosZ)
+        {
+            _count++;
+
+            _playerPosZ = PlayerPosition.position.z;
+        }
+
+        Counter.text = _count.ToString();
+    }
+}
